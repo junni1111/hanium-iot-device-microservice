d0d5217799b564b7fc4ca4cf46ecb23608a348e5
KGESH <baram789456@gmail.com>
2022-05-02 16:58:34 +0900

    임시 eslint

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 export const device_host = process.env.DEVICE_HOST || '0.0.0.0';
 export const device_port = Number(process.env.DEVICE_HOST) || 7779;
 export const mqtt_broker_url =
