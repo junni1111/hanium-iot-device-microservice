9b9900f9e55f9d33c95fb67173697f962f90c5c6
KGESH <baram789456@gmail.com>
2022-05-05 15:25:03 +0900

    Cache Module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { DeviceModule } from './device/device.module';
 import { ApiModule } from './api/api.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseService } from './config/database.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { CacheModuleModule } from './cache/cache.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(databaseService.getTypeOrmConfig()),
     DeviceModule,
     ApiModule,
+    CacheModuleModule,
     ScheduleModule.forRoot(),
   ],
 })
