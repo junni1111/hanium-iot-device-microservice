f8e6aed7536895e0a107d4fcf643b8e5eaeb3654
KGESH <baram789456@gmail.com>
2022-05-31 15:27:13 +0900

    remove util

--- src/device/device-temperature.service.ts
+++ src/device/device-temperature.service.ts
@@ -2,33 +2,23 @@ import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { MQTT_BROKER } from '../util/constants/constants';
 import { ClientProxy } from '@nestjs/microservices';
 import { Temperature } from './entities/temperature.entity';
-import { TemperaturePacketDto } from './dto/temperature-packet.dto';
 import { TemperatureRepository } from './repositories/temperature.repository';
-import { Interval } from '@nestjs/schedule';
 import { DeviceService } from './device.service';
 import { SlaveRepository } from './repositories/slave.repository';
 import { ITemperatureConfig } from './interfaces/slave-configs';
-import { createQueryBuilder } from 'typeorm';
-import { DoubleKeysMap } from '../util/double-keys-map';
 import { Cache } from 'cache-manager';
-import { LedPacketDto } from './dto/led-packet.dto';
 import { SlaveConfigDto } from '../api/dto/slave/slave-config.dto';
 import { ESlaveConfigTopic } from '../util/constants/api-topic';
 
 @Injectable()
 export class DeviceTemperatureService {
-  /** Todo: Redis로 걷어내면 제거 */
-  public readonly currentTemperatures: DoubleKeysMap;
-
   constructor(
     @Inject(MQTT_BROKER) private readonly mqttBroker: ClientProxy,
     @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
     private readonly temperatureRepository: TemperatureRepository,
     private readonly deviceService: DeviceService,
     private readonly slaveRepository: SlaveRepository,
-  ) {
-    this.currentTemperatures = new DoubleKeysMap();
-  }
+  ) {}
 
   async saveTemperature(temperature: Temperature) {
     try {
@@ -56,8 +46,6 @@ export class DeviceTemperatureService {
     } catch (e) {
       throw e;
     }
-    /* get cached temperature */
-    // return this.currentTemperatures.get([masterId, slaveId]);
   }
 
   async fetchTemperature(masterId: number, slaveId: number) {
@@ -94,33 +82,6 @@ export class DeviceTemperatureService {
 
   async createTestData(masterId: number, slaveId: number) {
     return this.temperatureRepository.createTestData(masterId, slaveId);
-  }
-
-  /**
-   * Todo: 이상 온도 감지하면 발생하는 트리거
-   *       추후 문자나 푸시 등 사용자에게 알림 기능 추가
-   *       지금은 임시로 마스터 보드의 LED를 1초정도 켰다 끔 */
-  mockOverRangeTrigger(masterId: number) {
-    console.log(`온도 범위 값 초과`);
-    const topic = `master/${masterId}/temperature`;
-
-    const mockLedMessage = new LedPacketDto(
-      0x23,
-      0x21,
-      0xff,
-      0xd1,
-      0x01,
-      0x20,
-      0x09,
-      [0xe1],
-    );
-
-    console.log(`send mock led message: `, mockLedMessage);
-
-    return this.deviceService.publishEvent(
-      topic,
-      JSON.stringify(mockLedMessage),
-    );
   }
 
   async cacheTemperature({ masterId, slaveId, temperature }: Temperature) {
