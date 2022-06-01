02f5102a919c607383b0cc831d61161527d1f3cd
KGESH <baram789456@gmail.com>
2022-06-02 01:31:46 +0900

    update dockerfile

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -11,30 +11,9 @@ import { ApiSlaveService } from './slave/api-slave.service';
 import { ApiWaterPumpController } from './water-pump/api-water-pump.controller';
 import { ApiLedController } from './led/api-led.controller';
 import { ApiThermometerController } from './thermometer/api-thermometer.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Temperature } from '../device/entities/temperature.entity';
-import { Humidity } from '../device/entities/humidity.entity';
-import { WaterPump } from '../device/entities/water-pump.entity';
-import { Led } from '../device/entities/led.entity';
-import { Master } from '../device/entities/master.entity';
-import { Slave } from '../device/entities/slave.entity';
-import { TemperatureRepository } from '../device/repositories/temperature.repository';
-import { MasterRepository } from '../device/repositories/master.repository';
-import { SlaveRepository } from '../device/repositories/slave.repository';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      Temperature,
-      Humidity,
-      WaterPump,
-      Led,
-      Master,
-      Slave,
-      TemperatureRepository,
-      MasterRepository,
-      SlaveRepository,
-    ]),
     CacheModule.register({
       store: redisStore,
       host: REDIS_HOST,
