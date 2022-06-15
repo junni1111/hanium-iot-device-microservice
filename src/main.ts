ae58c25179b5f70b1a92e130f7666770a213add2
KGESH <baram789456@gmail.com>
2022-06-15 13:14:21 +0900

    change port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
-import { DEVICE_HOST, DEVICE_PORT, MQTT_BROKER_URL } from './config/config';
+import {
+  DEVICE_HEALTH_PORT,
+  DEVICE_HOST,
+  DEVICE_PORT,
+  MQTT_BROKER_URL,
+} from './config/config';
 import { ValidationPipe } from '@nestjs/common';
 import { REDIS_HOST, REDIS_PORT } from './config/redis.config';
 
 async function bootstrap() {
   console.log(`Start ENV = `, process.env.NODE_ENV);
+  console.log(`ENV LIST: `, process.env);
   console.log(
     `Device Microservice Listening HOST:${DEVICE_HOST} PORT:${DEVICE_PORT} MQTT URL:${MQTT_BROKER_URL}...
       Redis Host: ${REDIS_HOST} Redis Port: ${REDIS_PORT}`,
@@ -36,9 +42,10 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
-  await app.listen(DEVICE_PORT, () => {
+  await app.listen(DEVICE_HEALTH_PORT, () => {
     console.log(
-      `Running Device Microservice. Listening HOST:${DEVICE_HOST} PORT:${DEVICE_PORT} MQTT URL:${MQTT_BROKER_URL}...
+      `Running Device Microservice. 
+      Listening HOST:${DEVICE_HOST} HEALTH_PORT:${DEVICE_HEALTH_PORT} DEVICE_PORT:${DEVICE_PORT} MQTT URL:${MQTT_BROKER_URL}...
       Redis Host: ${REDIS_HOST} Redis Port: ${REDIS_PORT}`,
     );
   });
