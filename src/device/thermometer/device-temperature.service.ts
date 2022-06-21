590ae112ad66e85e477f905d405ed6881ea9a399
KGESH <baram789456@gmail.com>
2022-06-21 18:03:08 +0900

    날짜 범위에 따른 온도 fetch test 작성

--- src/device/thermometer/device-temperature.service.ts
+++ src/device/thermometer/device-temperature.service.ts
@@ -11,6 +11,8 @@ import { SlaveConfigDto } from '../../api/dto/slave/slave-config.dto';
 import { ESlaveConfigTopic, ESlaveState } from '../../util/constants/api-topic';
 import { SensorConfigKey, SensorStateKey } from '../../util/key-generator';
 import { map } from 'rxjs';
+import { Between, createQueryBuilder } from 'typeorm';
+import { subDays, addDays } from 'date-fns';
 
 @Injectable()
 export class DeviceTemperatureService {
@@ -21,6 +23,17 @@ export class DeviceTemperatureService {
     private readonly deviceService: DeviceService,
     private readonly slaveRepository: SlaveRepository,
   ) {}
+
+  /** Todo: Custom Repository 제거하고
+   *        이 함수에 온도 저장 로직 설정 */
+  saveTemp(temp: Temperature) {
+    return this.temperatureRepository
+      .createQueryBuilder()
+      .insert()
+      .into(Temperature)
+      .values(temp)
+      .execute();
+  }
 
   async saveTemperature(temperature: Temperature) {
     try {
@@ -50,6 +63,26 @@ export class DeviceTemperatureService {
     } catch (e) {
       throw e;
     }
+  }
+
+  fetchTemperatureOneDay(
+    masterId: number,
+    slaveId: number,
+    beginDate: Date,
+    endDate: Date,
+  ) {
+    return createQueryBuilder()
+      .select(['create_at AS x', 'temperature AS y'])
+      .where(`master_id = :masterId`, { masterId })
+      .andWhere(`slave_id = :slaveId`, { slaveId })
+      .andWhere(`create_at BETWEEN :begin AND :end`, {
+        begin: beginDate,
+        end: endDate,
+      })
+      .distinct(true)
+      .from(Temperature, 'temperatures')
+      .orderBy('create_at', 'ASC')
+      .getRawMany();
   }
 
   async fetchTemperature(masterId: number, slaveId: number) {
