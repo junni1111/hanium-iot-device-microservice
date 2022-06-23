acfb350e8436e91b02a0d78a3c1f9c06c59c7145
KGESH <baram789456@gmail.com>
2022-06-24 01:35:59 +0900

    키 생성 테스트 작성

--- src/cache/redis.service.spec.ts
+++ src/cache/redis.service.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RedisController } from './redis.controller';
 import { RedisModule } from './redis.module';
-import { GenerateTemperatureKeys } from '../util/key-generator';
-import { addDays, addMinutes } from 'date-fns';
+import { GenerateAverageKeys } from '../util/key-generator';
+import { addDays } from 'date-fns';
 describe('Redis 컨트롤러 테스트', () => {
   let controller: RedisController;
 
@@ -27,14 +27,8 @@ describe('Redis 컨트롤러 테스트', () => {
     const BEGIN_DATE = new Date();
     const END_DATE = addDays(BEGIN_DATE, 5);
 
-    const expectFiveKeys = GenerateTemperatureKeys(
-      100,
-      100,
-      BEGIN_DATE,
-      END_DATE,
-      addDays,
-      1,
-    );
+    const expectFiveKeys = GenerateAverageKeys(100, 100, BEGIN_DATE, END_DATE, addDays, 1);
+    console.log(`result: `, expectFiveKeys);
 
     expect(expectFiveKeys.length).toEqual(5);
   });
