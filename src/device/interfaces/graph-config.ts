c4312ce56892edb685e261bc8858fc0f19deb0c6
KGESH <baram789456@gmail.com>
2022-06-24 01:35:13 +0900

    그래프 점 클래스 생성

--- src/device/interfaces/graph-config.ts
+++ src/device/interfaces/graph-config.ts
@@ -1,5 +1,16 @@
-export interface IGraphConfig {
-  x: string;
-  y: number;
+export class GraphPoint {
   etc: string;
+
+  constructor(
+    public readonly x: string,
+    public readonly y: number,
+    min: number,
+    max: number,
+  ) {
+    this.etc = this.contains(y, min, max) ? 'stable' : 'unstable';
+  }
+
+  private contains(average: number, min: number, max: number) {
+    return average >= min && average <= max;
+  }
 }
