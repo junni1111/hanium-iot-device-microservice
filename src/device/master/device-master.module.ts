a37e97a28b857b520099d3d4529b96d7c62e7b4b
KGESH <seonghyeon.jee@gmail.com>
2022-08-06 10:57:55 +0900

    CI: Fix Conflict

--- src/device/master/device-master.module.ts
+++ src/device/master/device-master.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
-import { MasterRepository } from './master.repository';
-=======
-import { MasterRepository } from '../repositories/master.repository';
->>>>>>> 87cc732c0a39fb2bf28c9b7c68c001b9fcdd02e7
 import { DeviceMasterService } from './device-master.service';
+import { MasterRepository } from './master.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MasterRepository])],
